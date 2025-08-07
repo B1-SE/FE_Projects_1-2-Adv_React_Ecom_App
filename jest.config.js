b1dcba82cc6ca28ccf52f79ec51e092d5cc58782
B1-SE <bajones2@coastal.edu>
2025-08-07 12:03:42 -0500

    second commit

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,11 @@
 // Jest config
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
